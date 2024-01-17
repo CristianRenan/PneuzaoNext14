0c88bca91682e2152048b146ae883659ab2a35f0
Cristian <cristian.freitas@pneufree.com.br>
2024-01-17 15:10:35 -0300

    teste

--- src/components/aplicacoesPneu/aplicacoesPneu.tsx
+++ src/components/aplicacoesPneu/aplicacoesPneu.tsx
@@ -13,7 +13,7 @@ export default function AplicacoesPneu() {
         </div>
         <div className=" w-28 flex flex-col items-center gap-2">
           <img src="https://pneuzao.com.br/wp-content/uploads/2023/09/utilitario-leve.svg" />
-          <text className="text-xs">UTILITÁRIOS LEVES</text>
+          <text className="text-xs">UTILITÁRIOS LEVESS</text>
         </div>
       </div>
       <text className="text-xs">
